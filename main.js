92b9b5c39480ba1c896b698cb5c01308d22824fe
Lucas Locatelli <lucas_locatelli2002@hotmail.com>
2024-11-08 14:05:40 -0300

    Began refactor, new functions added

--- main.js
+++ main.js
@@ -1,44 +1,67 @@
-var list = []
-const display = document.getElementById('display')
-var verify = true
+let list = [];
+const display = document.getElementById('display');
+let verify = true;
+let on = false;
+let count = 0;
+let memory;
 
-function findNum(a) {
-    if(verify == false) {
+function findNum(digit) {
+    if(!on) return;
+
+    if(!verify)
         clearAll()
-    }
 
     verify = true
     let writer = document.createElement('div')
-    writer.innerHTML = a
+    writer.innerHTML = digit
     display.appendChild(writer)
-    list.push(a)
-
+    list.push(digit)
 }
 
 function calculate() {
+    if(!on) return;
+
     let indexTimes = list.indexOf('X')
     list[indexTimes] = '*'
     let indexDivide = list.indexOf('÷')
     list[indexDivide] = '/'
-    console.log(list)
-    concatList = list.join('')
-    let result = eval(concatList) 
-    display.innerHTML = result
-    display.style.overflowX = 'auto'
+    let indexPercentage = list.indexOf('%')
+    list[indexPercentage] = '/100'
 
+    let concatList = list.join('')
+    display.innerHTML = memory = eval(concatList)
+    display.style.overflowX = 'auto'
     verify = false
 }
 
+function turnOn() {
+    on = true;
+}
+
+function turnOff() {
+    clearAll()
+    on = false
+}
+
 function clearAll() {
+    if(!on) {
+        turnOn()
+        return;
+    }
+
     display.innerHTML = " "
-    list = []
-    console.log(list)
+    list.clear()
     verify = true
     display.style.overflowX = 'hidden'
 }
 
 function reset() {
     display.removeChild(display.lastChild)
+}
+
+function recallMemory() {
+    list.clear();
+    list.push(memory)
 }
 
 
