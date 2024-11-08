f5b2d21423f2cd2e4934446ef9e534cf8c1651e6
Lucas Locatelli <lucas_locatelli2002@hotmail.com>
2024-11-08 19:33:43 -0300

    finished

--- main.js
+++ main.js
@@ -3,54 +3,37 @@ const display = document.getElementById('display');
 let verify = true;
 let on = false;
 let count = 0;
-let memory;
-
+let memory = '';
+let element = document.getElementById("memo");
+element.hidden = true;
 function findNum(digit) {
-    if(!on) return;
-
     if(!verify)
-        clearAll()
+        clearAll();
 
-    verify = true
-    let writer = document.createElement('div')
-    writer.innerHTML = digit
-    display.appendChild(writer)
-    list.push(digit)
+    verify = true;
+    let writer = document.createElement('div');
+    writer.innerHTML = digit;
+    display.appendChild(writer);
+    list.push(digit);
 }
 
 function calculate() {
-    if(!on) return;
 
-    let indexTimes = list.indexOf('X')
-    list[indexTimes] = '*'
-    let indexDivide = list.indexOf('÷')
-    list[indexDivide] = '/'
-    let indexPercentage = list.indexOf('%')
-    list[indexPercentage] = '/100'
+    let indexTimes = list.indexOf('X');
+    list[indexTimes] = '*';
+    let indexDivide = list.indexOf('÷');
+    list[indexDivide] = '/';
+    let indexPercentage = list.indexOf('%');
+    list[indexPercentage] = '/100';
 
-    let concatList = list.join('')
-    display.innerHTML = memory = eval(concatList)
-    display.style.overflowX = 'auto'
-    verify = false
-}
-
-function turnOn() {
-    on = true;
-}
-
-function turnOff() {
-    clearAll()
-    on = false
+    display.innerHTML = memory = eval(list.join(''));
+    display.style.overflowX = 'auto';
+    verify = false;
 }
 
 function clearAll() {
-    if(!on) {
-        turnOn()
-        return;
-    }
-
     display.innerHTML = " "
-    list.clear()
+    list = []
     verify = true
     display.style.overflowX = 'hidden'
 }
@@ -60,8 +43,40 @@ function reset() {
 }
 
 function recallMemory() {
-    list.clear();
-    list.push(memory)
+    count++;
+
+    if(count === 1) {
+        clearAll();
+        list.push(String(memory));
+        let writer = document.createElement('div');
+        writer.innerHTML = String(memory);
+        display.appendChild(writer);
+        element.hidden = false;
+    } else if(count === 2) {
+        memory = '';
+        count = 0;
+        element.hidden = true;
+    }
+}
+
+function calcMinusMemory() {
+    list.unshift(memory, '-');
+    calculate();
+}
+
+function calcPlusMemory() {
+    list.unshift(memory, '+');
+    calculate();
+}
+
+function calcSqrt() {
+    let num = eval(list.join(''));
+    let sqrt = Math.sqrt(num);
+    clearAll();
+    let writer = document.createElement('div');
+    writer.innerHTML = String(sqrt.toFixed(2));
+    display.appendChild(writer);
+
 }
 
 
